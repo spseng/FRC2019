dfe0b4dd7fa47a961b0583b0afc83db20a0ea014
twardrop <twardrop@sps.edu>
2018-01-26 14:56:21 -0500

    fix 1

---
+++ Tank Drive/src/org/usfirst/frc/team1512/robot/Robot - Copy.java
@@ -0,0 +1,35 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc.team1512.robot;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+/**
+ * This is a demo program showing the use of the RobotDrive class, specifically
+ * it contains the code necessary to operate a robot with tank drive.
+ */
+public class Robot extends IterativeRobot {
+	private Spark spark;
+	private Spark spark1;
+
+	@Override
+	public void robotInit() {
+		spark = new Spark(0);
+		spark1 = new Spark(1);
+	}
+
+	@Override
+	public void teleopPeriodic() {
+		spark.set(1.0);
+		spark1.set(-1.0);
+		// hello 
+	}
+}
