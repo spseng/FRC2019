2188335ac302846030a7aa911408589c0d2766e6
twardrop <twardrop@sps.edu>
2018-02-01 16:22:59 -0500

    TankDrive updates

--- Tank Drive/src/org/usfirst/frc/team1512/robot/Robot.java
+++ Tank Drive/src/org/usfirst/frc/team1512/robot/Robot.java
@@ -8,6 +8,7 @@
 package org.usfirst.frc.team1512.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
 //import com.ctre.phoenix.
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
@@ -32,16 +33,24 @@ public class Robot extends IterativeRobot {
 	private  XboxController xbox;
 	private WPI_TalonSRX FrontLeftMotor;
 	private WPI_TalonSRX FrontRightMotor;
+	private WPI_TalonSRX ArmMotor1;
 	private Potentiometer pot;
+	private double p1lowAngle;
+	private double p1middleAngle;
+	private double p1highAngle;
 	private AnalogInput ai;
 	private double degreesPot;
 
 	@Override
 	public void robotInit() {
+		p1lowAngle = 150;
+		p1middleAngle = 200;
+		p1highAngle = 250;
 		//m_leftStick = new Joystick(0);
 		//m_rightStick = new Joystick(1);
 		xbox = new XboxController(0);
 		FrontLeftMotor = new WPI_TalonSRX(2);
+		ArmMotor1 = new WPI_TalonSRX(3);
 		FrontRightMotor = new WPI_TalonSRX(4);
 		ai = new AnalogInput(3);
 		pot = new AnalogPotentiometer(ai, 360, 30);
@@ -64,9 +73,50 @@ public class Robot extends IterativeRobot {
 		}
 		
 		degreesPot = pot.get();
+		//40-390
 		
 		System.out.println(degreesPot);
 		
+		//low scale
+		if(xbox.getAButtonPressed()) {
+			if(pot.get()<p1lowAngle-5) {
+				ArmMotor1.set(0.2);
+			} else if (pot.get()>p1lowAngle+5) {
+				ArmMotor1.set(-0.2);
+			} else {
+				ArmMotor1.set(0.0);
+				
+			}
+		}
+		
+		//middle scale position
+		if(xbox.getBButtonPressed()) {
+			if(pot.get()<p1middleAngle-5) {
+				ArmMotor1.set(0.2);
+			} else if (pot.get()>p1middleAngle+5) {
+				ArmMotor1.set(-0.2);
+			} else {
+				ArmMotor1.set(0.0);
+				
+			}
+		}
+		//high scale position
+		if(xbox.getYButtonPressed()) {
+			if(pot.get()<p1highAngle-5) {
+				ArmMotor1.set(0.2);
+			} else if (pot.get()>p1highAngle+5) {
+				ArmMotor1.set(-0.2);
+			} else {
+				ArmMotor1.set(0.0);
+			}
+		}
+		
+		if(xbox.getXButtonPressed()) {
+			p1lowAngle = pot.get();
+		}
+		
 	}
-	
+		
 }
+	
+
