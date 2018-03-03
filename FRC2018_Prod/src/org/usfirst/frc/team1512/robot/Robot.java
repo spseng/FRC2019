1904cb15c3a275eed5f9760b700a4c4298e12b96
RyanRio <thermitchellman@gmail.com>
2018-03-03 00:56:06 -0500

    camera for command-based

--- FRC2018_Prod/src/org/usfirst/frc/team1512/robot/Robot.java
+++ FRC2018_Prod/src/org/usfirst/frc/team1512/robot/Robot.java
@@ -7,6 +7,7 @@
 
 package org.usfirst.frc.team1512.robot;
 
+import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
@@ -42,6 +43,7 @@ public class Robot extends TimedRobot {
 	 */
 	@Override
 	public void robotInit() {
+		CameraServer.getInstance().startAutomaticCapture();
 		driveTrain = new DriveTrain();
 		elevator = new Elevator();
 		grabber = new Grabber();
